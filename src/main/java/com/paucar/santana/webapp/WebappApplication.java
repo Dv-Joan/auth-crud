dca15bc423a769ac98ba9ef88533689c4eb59bc7
dv-joan <joan300501@gmail.com>
2023-07-23 15:18:35 -0500

    Habilitando CORS para poder consumir la API

--- src/main/java/com/paucar/santana/webapp/WebappApplication.java
+++ src/main/java/com/paucar/santana/webapp/WebappApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WebappApplication {
@@ -12,8 +14,19 @@ public class WebappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
 	}
+
 	@Bean
-	public PasswordEncoder passwordEncoder(){
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+			}
+		};
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder(15);
 	}
 }
--- src/main/java/com/paucar/santana/webapp/config/CORS_Enabler.java
+++
@@ -1,5 +0,0 @@
-package com.paucar.santana.webapp.config;
-
-public class CORS_Enabler {
-
-}
