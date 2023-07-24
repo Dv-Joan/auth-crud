5ba877f83187285d3a85fc64d7a3a46a0ac9e5c0
dv-joan <joan300501@gmail.com>
2023-07-24 15:56:08 -0500

    Login Terminado

--- src/main/java/com/paucar/santana/webapp/controller/TeacherController.java
+++ src/main/java/com/paucar/santana/webapp/controller/TeacherController.java
@@ -8,32 +8,38 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+// ! El ENDPOINT para utilizar la API
 @RequestMapping("api/docentes")
 public class TeacherController {
 
     @Autowired
     private TeacherService TeacherService;
 
+    // ? ENDPOINT para agregar un Docente
     @PostMapping("/agregar")
     public Teacher addTeacher(@RequestBody Teacher Teacher) {
         return TeacherService.addTeacher(Teacher);
     }
 
+    // ? ENDPOINT para buscar un Docente por ID
     @GetMapping("/{TeacherId}")
     public Teacher getTeacher(@PathVariable("TeacherId") Integer TeacherId) {
         return TeacherService.getTeacher(TeacherId);
     }
 
+    // ? ENDPOINT para listar todos los Docentes
     @GetMapping
     public List<Teacher> getAllTeachers() {
         return TeacherService.getAllTeachers();
     }
 
+    // ? ENDPOINT para eliminar un Docente por su ID
     @DeleteMapping("{TeacherId}")
     public void deleteTeacher(@PathVariable("TeacherId") Integer TeacherId) {
         TeacherService.deleteTeacher(TeacherId);
     }
 
+    // ? ENDPOINT para actualizar los Docentes por su ID
     @PutMapping(path = "/{TeacherId}")
     public Teacher updateTeacher(
             @PathVariable Integer TeacherId,
--- src/main/java/com/paucar/santana/webapp/security/AppSecurityConfig.java
+++ src/main/java/com/paucar/santana/webapp/security/AppSecurityConfig.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-
-//Modulos de la dependencia Spring Security para la Autenticacion y Autorizacion de 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,64 +17,46 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import static com.paucar.santana.webapp.security.Roles.*;
 
 @Configuration
-
-// Activacion de la configuracion de Spring Security
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
-        // Modulo PasswordEncoder para la encriptacion de la contraseña
-        @Autowired
         private final PasswordEncoder passwordEncoder;
 
+        @Autowired
         public AppSecurityConfig(PasswordEncoder passwordEncoder) {
                 this.passwordEncoder = passwordEncoder;
         }
 
-        /*
-         * Configuracion de la Autorizacion de las peticiones HTTP de la API REST de
-         * Docentes con sus respectivos roles y operaciones permitidas para cada uno
-         */
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-
-                /*
-                 * Manejo de inicio de sesiones para los usuarios, esto hace que al actualizar
-                 * la pagina el usuario siga autenticado
-                 */
-                http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-                /*
-                 * Configuracion de las perticiones Http permitidas para cada uno de los roles
-                 * definidos previamente
-                 */
                 http
-                                .csrf().disable()
-                                .authorizeHttpRequests()
-                                /*
-                                 * Los antMatchers son las rutas de la API REST de Docentes con sus respectivos
-                                 * roles y operaciones permitidas para cada uno
-                                 */
+                                .sessionManagement(management -> management
+                                                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                                .authorizeRequests()
                                 .antMatchers(HttpMethod.DELETE, "/api/docentes/{TeacherId}").hasRole(DECANO.name())
                                 .antMatchers(HttpMethod.PUT, "/api/docentes/{TeacherId}").hasRole(DECANO.name())
-                                .antMatchers("/api/docentes/add")
-
-                                // Operaciones permitidas para los roles de Secretaría y Docente
-                                .hasAnyRole(DECANO.name(), SECRETARÍA.name())
+                                .antMatchers("/api/docentes/add").hasAnyRole(DECANO.name())
                                 .antMatchers("/api/docentes")
                                 .hasAnyRole(DECANO.name(), SECRETARÍA.name(), DOCENTE.name())
-                                .antMatchers("/api/docentes{TeacherId}")
+                                .antMatchers("/api/docentes/{TeacherId}")
                                 .hasAnyRole(DECANO.name(), SECRETARÍA.name(), DOCENTE.name())
-                                .anyRequest()
-
-                                // Verificacion de la Autenticacion
-                                .authenticated()
+                                .anyRequest().authenticated()
+                                .and()
+                                .formLogin()
+                                .loginPage("/login")
+                                .permitAll()
+                                .defaultSuccessUrl("/dashboard", true)
+                                .and()
+                                .logout()
+                                .logoutUrl("/logout")
+                                .logoutSuccessUrl("/login")
+                                .permitAll()
                                 .and()
                                 .httpBasic();
         }
 
         @Bean
         @Override
-        // Credenciales para los diferentes usuarios
         protected UserDetailsService userDetailsService() {
                 UserDetails Jaime = User.builder()
                                 .username("Jaime")
@@ -96,9 +76,6 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                                 .roles(DECANO.name())
                                 .build();
 
-                InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager(Jaime, Rebeca, Hector);
-
-                return userDetailsManager;
+                return new InMemoryUserDetailsManager(Jaime, Rebeca, Hector);
         }
-
 }
